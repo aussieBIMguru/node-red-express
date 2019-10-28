530421cede16c9b53d75df7591f38125d8e29996
Gary Law <info@skinv1.co.ukl>
2019-10-29 01:36:17 +0800

    Email pdf investigation

--- bimlauncher-email2pdf/test.js
+++ bimlauncher-email2pdf/test.js
@@ -2,7 +2,7 @@ const { renderThreadToPDF, getAllMailIds } = require(".");
 var t = require("./data.json");
 
 (async () =>  {
-    console.log(await renderThreadToPDF(t, "/tmp/test2.pdf"))
+    console.log(await renderThreadToPDF(t, "./test2.pdf"))
     console.log(getAllMailIds(t));
 })()
 
